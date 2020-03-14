1e5616f7cf8de33aa84e03796321356a8d69710d
Monika Frankowska <monia.frankowska@gmail.com>
2020-03-14 17:59:20 +0100

    error handling middleware

--- app.js
+++ app.js
@@ -16,16 +16,28 @@ app.get('/', (req,res)=> {
     res.render('index', {projects});
 });
 //An about route
-app.get('/about', (req,res)=> {
+app.get('/about', (req, res)=> {
     res.render('about');
 });
 
 //Dynamic projects route
-app.get('/project/:id', (req,res,next)=> {
+app.get('/project/:id', (req, res, next)=> {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId );
     res.render('project', {project});
 });
+
+//error handling
+
+app.get('*', function(req, res, next) {
+    let err = new Error('Page Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+app.use((err, req, res, next)=> {
+    res.locals.error = err;
+    res.render('error');
+} );
 
 //Starting a server on port 3000
 app.listen(3000, ()=> {

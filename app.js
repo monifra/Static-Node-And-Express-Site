785718f732695d6bc6b1bf67a0dbd23271f72888
Monika Frankowska <monia.frankowska@gmail.com>
2020-03-17 13:39:30 +0100

    heroku

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use((err, req, res, next)=> {
 } );
 
 //Starting a server on port 3000
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log('The application is running on localhost:3000');
 } );

14e9a322ca0c74d606df5d49febd69ba14cca337
Monika Frankowska <monia.frankowska@gmail.com>
2020-06-04 20:16:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ app.get('/', (req,res)=> {
 //An about route
 app.get('/about', (req, res)=> {
     res.render('about');
-});
+} );
 
 //Dynamic projects route
 app.get('/project/:id', (req, res, next)=> {
     const projectId = req.params.id;
+
     if(projectId < projects.length){ //if choosen project id exist
         const project = projects.find( ({ id }) => id === +projectId );
         res.render('project', {project}); //renderer choosen project page
@@ -32,19 +33,23 @@ app.get('/project/:id', (req, res, next)=> {
         err.statusCode = 404;
         next(err);
     }
-});
+} );
 
 //error handling
 app.get('*', function(req, res, next) { //for any not existing rout
     let err = new Error('Page Not Found'); //create 404 status error
     err.statusCode = 404;
     next(err);
-});
+} );
+
+
 app.use((err, req, res, next)=> {
     res.locals.error = err;
     console.log('Error: Something went wrong');
     res.render('error'); //render error page
 } );
+
+
 
 //Starting a server on port 3000
 app.listen(process.env.PORT || 3000, ()=> {
